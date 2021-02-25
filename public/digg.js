d17116fb1a5304f92dd890b85a03e41cf7e1cedc
Linulas <linubran90@gmail.com>
2021-02-25 12:51:29 +0100

    reset menu display on click outside

--- public/digg.js
+++ public/digg.js
@@ -1,8 +1,8 @@
 const focusableElements = 'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])';
 
 // Skips all menu items and sets focus on first focusable element in main
-$('.skip-to-content').on('click', function (event) {
-    event.preventDefault();
+$('.skip-to-content').on('click', ev => {
+    ev.preventDefault();
 
     let content = $('#content');
 
@@ -19,7 +19,7 @@ $('.skip-to-content').on('click', function (event) {
 });
 
 // Make mobile menu visible when clicked, and trap focus
-$('#mobile-menu').on('click', function (event) {
+$('#mobile-menu').on('click', ev => {
     const menu = $('#menu');
     menu.css('display', `${"block"}`);
     trapFocus(menu[0]);
@@ -27,7 +27,7 @@ $('#mobile-menu').on('click', function (event) {
 });
 
 // Make mobile chats visible when clicked, and trap focus
-$('#mobile-chats').on('click', function (event) {
+$('#mobile-chats').on('click', ev => {
     const chats = $('#chats-menu');
     chats.css('display', `${"block"}`);
     trapFocus(chats[0]);
@@ -35,7 +35,7 @@ $('#mobile-chats').on('click', function (event) {
 });
 
 // Make the user menu accessible with enter key and traps focus
-$('#user_label').keypress(function (e) {
+$('#user_label').keypress(e => {
     var key = e.which;
     if (key == 13)  // the enter key code
     {
@@ -44,6 +44,11 @@ $('#user_label').keypress(function (e) {
         trapFocus(userControls[0]);
         skipToElement(userControls[0].querySelectorAll(focusableElements));
     }
+});
+
+// Force a display reset on menu if its hidden with click outside
+$('main').on('click', ev => {
+    $('html').hasClass('slideout-open') && clickOutside($('#menu')[0]);
 });
 
 /**
@@ -55,12 +60,24 @@ function setDisplayOnMobileMenus() {
     $('#chats-menu').css('display', `${!menuOpen ? "none" : "block"}`);
 }
 
+
+/**
+ * Sets focus on the selected element
+ *
+ * @param {*} element focus this element
+ * @param {boolean} [setTabIndex=false] set to true if element does not have tabindex
+ */
 function skipToElement(element, setTabIndex = false) {
     setTabIndex && element.attr('tabindex', '-1');
     element[0] && element[0].focus();
 }
 
 
+/**
+ * Programatically click on the parent of a given element
+ *
+ * @param {*} element click the parent of this element
+ */
 function clickOutside(element) {
     const parent = element.parentElement;
     parent ? parent.click() : console.log('click outside failed');
@@ -82,7 +99,7 @@ function trapFocus(element) {
     const KEYCODE_TAB = 9;
     const KEYCODE_ESCAPE = 27;
 
-    element.addEventListener('keydown', function (e) {
+    element.addEventListener('keydown', e => {
         const tabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
         const escapePressed = (e.key === 'Escape' || e.keyCode === KEYCODE_ESCAPE);
 
@@ -97,6 +114,10 @@ function trapFocus(element) {
                 lastFocusable.focus();
                 e.preventDefault();
             }
+            if (document.activeElement.tabIndex == "-1") {
+                firstFocusable.focus();
+                e.preventDefault();
+            }
         } else /* tab */ {
             if (document.activeElement === lastFocusable) {
                 firstFocusable.focus();
@@ -106,4 +127,5 @@ function trapFocus(element) {
     });
 }
 
+// Initialize css display on menus
 setDisplayOnMobileMenus();
